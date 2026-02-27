4051832864434c8d861a8d39b49717213e1c172b
Andrew Embler <andrew@concrete5.org>
2026-02-27 05:53:40 -0800

    missed file

---
+++ concrete/elements/block_area_block_target.php
@@ -0,0 +1,13 @@
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+
+$resolvedAfterBlockId = isset($afterBlockId) ? (int) $afterBlockId : 0;
+$resolvedTargetIndex = isset($targetIndex) ? (int) $targetIndex : 0;
+?>
+
+<concrete-area-block-target
+    area-id="<?=$a->getAreaID()?>"
+    area-handle="<?=h($a->getAreaHandle())?>"
+    after-block-id="<?=$resolvedAfterBlockId?>"
+    target-index="<?=$resolvedTargetIndex?>"
+></concrete-area-block-target>
